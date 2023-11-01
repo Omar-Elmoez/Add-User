6c146368d0316026c293a0e122aebb0890a571b4
Omar Elmoez <omarelmoez2000@gmail.com>
2023-11-01 10:27:57 +0200

    add validateValues function and error state

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,39 @@ import ErrorModal from "./components/error-modal/ErrorModal";
 
 function App() {
   const [data, setData] = useState(null);
-  const [showError, setShowError] = useState(false); 
-  const [enteredValues, setEnteredValues] = useState({});
+  const [error, setError] = useState(null);
 
   const displayUsers = (userInputs) => {
-    setEnteredValues(userInputs)
-    if (+userInputs.age <= 0 || userInputs.username.trim().length === 0) {
-      handleError();
-    } else {
+    if (validateValues(userInputs)) {
       setData((prevData) => {
         return prevData ? [...prevData, userInputs] : [userInputs];
       });
     }
   };
 
-  const handleError = () => {
-    setShowError(!showError);
+  const validateValues = (values) => {
+    if (values.username.trim().length === 0 && values.age.trim().length === 0) {
+      setError({
+        title: "Invalid Input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
+    } else if (+values.age <= 0) {
+      setError({
+        title: "Invalid Age",
+        message: "Age must be greater than zero.",
+      });
+    } else if (values.username.trim().length === 0) {
+      setError({
+        title: "Invalid Name",
+        message: "Name cannot be empty.",
+      });
+    } else {
+      return true;
+    }
+  };
+
+  const hideErrorMsg = () => {
+    setError(null);
   };
 
   const msgStyle = {
@@ -33,10 +50,11 @@ function App() {
     <>
       <AddUser onSubmit={displayUsers} />
       {data ? <UsersList data={data} /> : <p style={msgStyle}>No Users Yet!</p>}
-      {showError && (
+      {error && (
         <ErrorModal
-          handleError={handleError}
-          enteredValues={enteredValues}
+          title={error.title}
+          message={error.message}
+          onConfirm={hideErrorMsg}
         />
       )}
     </>
--- src/components/error-modal/ErrorModal.jsx
+++ src/components/error-modal/ErrorModal.jsx
@@ -3,30 +3,14 @@ import Button from "../button/Button";
 import classes from "./ErrorModal.module.css";
 
 function ErrorModal(props) {
-  console.log("from error modal", props.enteredValues);
-  const hideErrorMsg = () => {
-    props.handleError();
-  };
-  let msgText = "";
-  if (
-    props.enteredValues.username.trim().length === 0 &&
-    props.enteredValues.age.trim().length === 0
-  ) {
-    msgText = "Please enter a valid name and age (non-empty values).";
-  } else if (+props.enteredValues.age <= 0) {
-    msgText = "Please enter a valid age ( > 0 )";
-  } else if (props.enteredValues.username.trim().length === 0) {
-    msgText = "Please enter a valid username (non-empty value).";
-  }
-
   return (
     <>
-      <div className={classes.wrap} onClick={hideErrorMsg}></div>
+      <div className={classes.wrap} onClick={props.onConfirm} />
       <div className={classes.msgBx}>
-        <header>Invalid Input</header>
+        <header>{props.title}</header>
         <div>
-          <p>{msgText}</p>
-          <Button onClick={hideErrorMsg}>Okay</Button>
+          <p>{props.message}</p>
+          <Button onClick={props.onConfirm}>Okay</Button>
         </div>
       </div>
     </>
